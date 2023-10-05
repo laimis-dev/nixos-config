7ed115e0fb6dc55abfb6f86854f05c5b405c4e2b
katinas15 <laimonas.janutenas@gmail.com>
2023-10-06 01:42:37 +0300

    trying multiseat assignment

--- multiseat.nix
+++ multiseat.nix
@@ -1,12 +1,7 @@
-let
-  isMultiSeatEnabled = true;  # or false depending on your need
-in
+{ ... }:
+
 {
-  services.udev.extraRules =
-    if isMultiSeatEnabled then ''
-      SUBSYSTEM=="drm", KERNEL=="card0", TAG+="seat", ENV{ID_SEAT}="seat1"
-      SUBSYSTEM=="sound", KERNEL=="card0", TAG+="seat", ENV{ID_SEAT}="seat1"
-      SUBSYSTEM=="usb", KERNEL=="usb1", TAG+="seat", ENV{ID_SEAT}="seat1"
-      SUBSYSTEM=="usb", KERNEL=="usb2", TAG+="seat", ENV{ID_SEAT}="seat1"
-    '' else "";
+  services.udev.extraRules = builtins.readFile "/etc/nixos/seat.rules";
+
 }
+
