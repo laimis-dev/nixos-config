dd4f5efdf9048dcdcc41d8d56c5121304f992f73
katinas15 <laimonas.janutenas@gmail.com>
2023-10-05 21:34:50 +0300

    inital commit

---
+++ nvidia.nix
@@ -0,0 +1,23 @@
+{ config, lib, pkgs, ... }:
+{
+
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    nvidiaPersistenced = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+   hardware.nvidia.prime = {
+    sync.enable = true;
+    nvidiaBusId = "PCI:1:0:0";
+    amdgpuBusId = "PCI:6:0:0";
+   };
+}
