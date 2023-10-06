b5e1c311aaa76694b7c1b0bb5c5e770384e88a2b
katinas15 <laimonas.janutenas@gmail.com>
2023-10-06 18:44:06 +0300

    multiseat fix

--- multiseat.nix
+++ multiseat.nix
@@ -1,14 +1,13 @@
 { lib, ... }:
 
 let
-  isMultiseatEnabled = true;
+  isMultiseatEnabled = false;
 in
 
 {
   services.udev = lib.optionalAttrs isMultiseatEnabled {
     extraRules = builtins.readFile "/etc/nixos/seat.rules";
   };
-
-  services.xserver.videoDrivers = if isMultiseatEnabled then ["nvidia" "amdgpu"] else ["nvidia"];
+  services.xserver.videoDrivers = ["nvidia"] ++ lib.optional isMultiseatEnabled "amdgpu";
 }
 
