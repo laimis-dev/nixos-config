b5f592cdac429c8fd61c14805a28d8614db3a52b
katinas15 <laimonas.janutenas@gmail.com>
2023-11-26 16:19:55 +0200

    polybar config

---
+++ packages/polybar/start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+killall -q polybar
+
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+for m in $(xrandr --listactivemonitors | grep "+" | cut -d" " -f6); do
+    MONITOR=$m polybar -q -r top -c ~/.config/polybar/polybar.ini
+done
